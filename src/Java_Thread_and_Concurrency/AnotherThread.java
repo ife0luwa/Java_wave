554d0b2df1a80a3e96847872f9fcb3ea54405594
mac <clementolu07@gmail.com>
2022-07-23 23:09:01 +0100

    Worked on Java multi-threading and concurrency

--- src/Java_Thread_and_Concurrency/AnotherThread.java
+++ src/Java_Thread_and_Concurrency/AnotherThread.java
@@ -8,13 +8,15 @@ public class AnotherThread extends Thread{
     public void run() {
         System.out.println(ANSI_GREEN + "Hello from " + currentThread().getName());
 
-        try{ Thread.sleep(3000);
+        try{ Thread.sleep(3000);  /* call Thread to sleep */
         }
         catch (InterruptedException e) {
             System.out.println(ANSI_GREEN + "another thread woke me up");
             return;
         }
+
         System.out.println(ANSI_GREEN + "3 seconds have passed, now I'm awake");
+
         for(int i = 0; i < 2; i++) {
         System.out.println("tell me what you want");
         }
---
+++ src/Java_Thread_and_Concurrency/ProducerConsumer/Main.java
@@ -0,0 +1,82 @@
+package Java_Thread_and_Concurrency.ProducerConsumer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static Java_Thread_and_Concurrency.ProducerConsumer.Main.EOF;
+
+public class Main {
+    public static final String EOF = "EOF";
+
+    public static void main(String[] args) {
+        List<String> buffer = new ArrayList<>();
+        MyProducer producer = new MyProducer(buffer, ColorOfThread.ANSI_PURPLE);
+        MyConsumer consumer1 = new MyConsumer(buffer, ColorOfThread.ANSI_RED);
+        MyConsumer consumer2 = new MyConsumer(buffer, ColorOfThread.ANSI_GREEN);
+
+        new Thread(producer).start();
+        new Thread(consumer1).start();
+        new Thread(consumer2).start();
+
+    }
+}
+
+class MyProducer implements Runnable {
+    private List<String> buffer;
+    private String color;
+
+    public MyProducer(List<String> buffer, String color) {
+        this.buffer = buffer;
+        this.color = color;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        String [] nums = {"1", "2", "3", "4", "5"};
+
+        for(String num : nums) {
+            try {
+                System.out.println(color + "adding..." + num);
+                synchronized(buffer) {
+                buffer.add(num);
+                }
+                Thread.sleep(random.nextInt(1000));
+            } catch (InterruptedException e) {
+
+            }
+        }
+        System.out.println(color + "Adding EOF and exiting...");
+        synchronized(buffer) {
+        buffer.add("EOF");
+        }
+    }
+}
+
+class MyConsumer implements Runnable{
+    private List<String> buffer;
+    private String color;
+
+    public MyConsumer(List<String> buffer, String color) {
+        this.buffer = buffer;
+        this.color = color;
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            synchronized(buffer) {
+            if(buffer.isEmpty()) {
+                continue;
+            }
+            if(buffer.get(0).equals(EOF)) {
+                System.out.println(color + "Exiting");
+                break;
+            } else {
+                System.out.println(color + "Removed " + buffer.remove(0));
+            }
+            }
+        }
+    }
+}
